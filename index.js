e24bc1a7fc037e54847ac47df6a7af8428c2e980
beatboxerish <ishan1996nangia@gmail.com>
2023-02-10 16:33:34 +0530

    changes to index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let myDocs = []
+let myDocs = [1,2,3,4]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById('ul-el')
@@ -10,5 +10,5 @@ inputBtn.addEventListener("click", function(){
 
 for (let i=0; i<myDocs.length; i++){
 	console.log(myDocs[i])
-	ulEl.textContent(myDocs[i])
+	ulEl.innerHTML += "<li>" + myDocs[i] + "</li>"
 }
