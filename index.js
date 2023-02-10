bc75df5bb99b7fac751f8f81974fdf391697cc3e
beatboxerish <ishan1996nangia@gmail.com>
2023-02-10 18:05:53 +0530

    Allow persisting in localStorage

--- index.js
+++ index.js
@@ -3,9 +3,25 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById('ul-el')
 
+let docsFromLocalStorage = JSON.parse(localStorage.getItem('myDocs'))
+let boolDocsFromLocalStorage = Boolean(docsFromLocalStorage)
+if (boolDocsFromLocalStorage){
+	myDocs = docsFromLocalStorage
+	renderDocs()
+}
+
 inputBtn.addEventListener("click", function(){
+	
+	// push input into myDocs
 	myDocs.push(inputEl.value)
+	
+	// clear input textbox
 	inputEl.value = ""
+
+	// set myDocs in localStorage
+	localStorage.setItem("myDocs", JSON.stringify(myDocs))
+
+	// call renderDocs
 	renderDocs()
 })
 
@@ -23,3 +39,9 @@ function renderDocs(){
 	}
 	ulEl.innerHTML = listItems
 }
+
+// let myDocs = `["something"]`
+// myDocs = JSON.parse(myDocs)
+// myDocs.push('something new')
+// myDocs = JSON.stringify(myDocs)
+// console.log(typeof myDocs)
